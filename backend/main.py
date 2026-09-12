e29bb8b47f516bfa68770ea15190685b2a9aeec5
Fernando <fernan2dt@gmail.com>
2026-09-12 16:10:30 -0300

    agrega CORS para diofanto.xyz

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="Diofanto API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+     allow_origins=["https://diofanto.xyz", "https://www.diofanto.xyz"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
